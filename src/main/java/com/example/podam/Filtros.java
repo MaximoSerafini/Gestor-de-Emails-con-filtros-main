40164732ce150219438d5d5a1bdc59cdabf28527
Santiago <sant.serrano@icloud.com>
2024-09-24 19:36:24 -0300

    updates

---
+++ src/main/java/com/example/podam/Filtros.java
@@ -0,0 +1,13 @@
+package com.example.podam;
+
+public class Filtros {
+
+    public void FiltrarAsunto (String asunto){
+
+    }
+
+    public void FiltrarNombre (String name){
+        
+    }
+    
+}
