10438818aa11825c03f10162a521e9c99e88594e
DiMaxi <maximoserafini44@gmail.com>
2024-09-24 18:22:47 -0300

    ahorasiperdon

--- src/test/java/com/example/podam/AppTest.java
+++ src/test/java/com/example/podam/AppTest.java
@@ -8,31 +8,34 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    extends TestCase{
+      // Test Esperado
+    //public static void main( String[] args ){
+    //    persona1 = new Contacto(“Nombre 1 Apellido 1”, “persona1@demo.com”)
+    //    persona2 = new Contacto(“Nombre 2 Apellido 2”, “persona2@demo.com”)
+//
+    //    persona3 = new Contacto(“Nombre 3 Apellido 3”, “persona3@demo.com”)
+    //    Email = new Email()
+    //    email.Asunto = “Prueba UCP”
+    //    email.Contendo = “Estamos en la materia Paradigmas 2”
+    //    email.Remitente = persona1
+    //    email.Para.Agregar(persona2)
+    //    emailManager.Enviar(email)
+    //    emailEnviado = emailManager.BandejaEnviados[0]
+    //    emailManager.BandejaEntrada.Agregar(email1)
+    //    emailManager.BandejaEntrada.Agregar(email2)
+    //    emailManager.BandejaEntrada.Agregar(email…N)
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    //    predicado1 = {Predicado para filtros de la UCP}
+    //    filtro1 = new Filtro(“Correos de la UCP”, predicado1)
+    //    filtro2 = new Filtro(“Del remitente Director/a”, predicado2 )
+    //    filtro1.Add(filtro2); //Aqui deberia unir predicados para hacer que filtro1 se
+    //    complejo
+    //    Var filtro3 = new Filtro(“Complejo 1”, filtro1.Predicado);
+    //    correosEncontradosDeLaUCP = emailManager.BandejaEntrada.Buscar(filtro1 )
+    //    Foreach (e in correosEncontrados ){
+    //        …verificar que se encuentra los emails
+    //    }
+    
+    //}
 }
