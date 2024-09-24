4f501cd97d5e5894288f1eff95fc0774bc9397ca
DiMaxi <maximoserafini44@gmail.com>
2024-09-24 17:31:10 -0300

    first commit

---
+++ src/main/java/com/example/podam/App.java
@@ -0,0 +1,14 @@
+package com.example.podam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //hola
+    }
+}
