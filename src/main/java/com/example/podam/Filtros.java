18ab0337a05b577b774caa402c434726db453d47
Santiago <sant.serrano@icloud.com>
2024-09-27 09:10:24 -0300

    filtro asunto

--- src/main/java/com/example/podam/Filtros.java
+++ src/main/java/com/example/podam/Filtros.java
@@ -1,13 +1,17 @@
 package com.example.podam;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Filtros {
 
-    public void FiltrarAsunto (String asunto){
-
-    }
-
-    public void FiltrarNombre (String name){
+    public static List<Email> filtroAsunto(List<Email> emails, String filtro) {
+    
+        Predicate<Email> porAsunto = email -> email.getAsunto().contains(filtro);
         
+        return emails.stream()
+                     .filter(porAsunto)
+                     .collect(Collectors.toList());
     }
     
 }
