049cb168ed81a8273b3021d0e923a0a51bdc1374
DiMaxi <maximoserafini44@gmail.com>
2024-10-06 15:03:28 -0300

    Test enviar email

--- src/test/java/com/example/podam/TestEmails1.java
+++ src/test/java/com/example/podam/TestEmails1.java
@@ -370,6 +370,31 @@ public class TestEmails1{
         assertEquals(email1, bandeja2.getBandejaEntrada().get(0));
         assertEquals(1, bandeja3.getBandejaEntrada().size());
         assertEquals(email1, bandeja3.getBandejaEntrada().get(0));
-
     }
+
+    @Test
+    public void EnviarEmail() {
+        Contacto contacto1 = new Contacto("Lucas", "lucas@gmail.com");
+        Contacto contacto2 = new Contacto("Maxi", "maxi@gmail.com");
+
+        EmailManager bandeja1 = new EmailManager(contacto1); // Remi
+        EmailManager bandeja2 = new EmailManager(contacto2); // Desti
+
+        Email email1 = new Email();
+        email1.setAsunto("Ayuda");
+        email1.setContenido("Prueba");
+        email1.setRemitente(contacto1);
+        email1.setPara(Arrays.asList(contacto2)); 
+
+        bandeja1.enviarEmail(email1, Arrays.asList(contacto2), Arrays.asList(bandeja1, bandeja2));
+
+        assertEquals(1, bandeja1.getBandejaSalida().size());
+        assertEquals(email1, bandeja1.getBandejaSalida().get(0));
+
+        assertEquals(1, bandeja2.getBandejaEntrada().size());
+        assertEquals(email1, bandeja2.getBandejaEntrada().get(0));
+    }
+
+
+    
 }
