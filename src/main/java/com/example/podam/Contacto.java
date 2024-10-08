c27b54d09ca38f8b562ca26fb3c3ab2aea32e6ab
gabval <ositofagundez123@gmail.com>
2024-10-08 05:34:48 -0300

    comentario de codigo para la clase contacto

--- src/main/java/com/example/podam/Contacto.java
+++ src/main/java/com/example/podam/Contacto.java
@@ -1,22 +1,23 @@
 package com.example.podam;
 
 public class Contacto {
-    private String name;
-    private String mail;
+    private String name;  // Nombre del contacto
+    private String mail;  // Correo electrónico
 
     public Contacto() {
-    }
+    }  // Constructor por defecto
 
     public Contacto(String name, String mail) {
         this.name = name;
         this.mail = mail;
-    }
+    }  // Constructor con parámetros
 
     public String getName() {
         return name;
-    }
+    }  // Getter para name
 
     public String getMail() {
         return mail;
-    }
+    }  // Getter para mail
 }
+
