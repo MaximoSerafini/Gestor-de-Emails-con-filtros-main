d28142e6d5fdcf5c24699329e93028668ac417b9
gabval <ositofagundez123@gmail.com>
2024-10-29 12:33:20 -0300

    desde ahora los test se van hacer en el reciente creado TestEmail2

---
+++ src/test/java/com/example/TestEmails2.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class TestEmails2 {
+
+    @Test
+
+    public void TestFavorito(){
+
+        
+    }
+
+
+    
+}
